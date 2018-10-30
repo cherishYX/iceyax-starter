03d9fdc691d6ba156712e3c6feeccd16156e353f
yanx <123190875@qq.com>
2018-10-30 14:07:08 +0800

    commit

--- src/main/java/cn/iceyax/base/mvc/BaseMapper.java
+++ src/main/java/cn/iceyax/base/mvc/BaseMapper.java
@@ -1,9 +1,5 @@
 package cn.iceyax.base.mvc;
 
-import java.util.List;
-
-import cn.iceyax.base.req.BaseReqPageVO;
-
 /**
  * 
  * ClassName: BaseMapper 
@@ -12,7 +8,7 @@ import cn.iceyax.base.req.BaseReqPageVO;
  * @email 123190875@qq.com
  * @date 2018年9月14日 下午3:39:20
  */
-public interface BaseMapper<T extends BaseEntity,Q extends BaseReqPageVO> {
+public interface BaseMapper<T extends BaseEntity> {
 	
 	int add(T t);
 	
@@ -22,7 +18,4 @@ public interface BaseMapper<T extends BaseEntity,Q extends BaseReqPageVO> {
 	
 	T get(T t);
 	
-	List<T> list(Q q);
-	
-	List<T> listByCondition(Q q);
 }
--- src/test/java/cn/iceyax/BaseTest.java
+++ src/test/java/cn/iceyax/BaseTest.java
@@ -26,12 +26,12 @@ public class BaseTest {
 		PackageInfo packageInfo = new PackageInfo();
 		packageInfo.setAuthor("yanx");
 		packageInfo.setDaoPackage("dao");
-		packageInfo.setProjectPath("");
+		packageInfo.setProjectPath("D:\\yxworkspace\\iceyax-boot-config");
 		packageInfo.setJavaPath("src\\main\\java");
 		packageInfo.setBasePackage("cn.ice.web");
 		packageInfo.setEntityPackage("entity");
 		packageInfo.setResourcePath("src\\main\\java");
-		packageInfo.setExtendsPath("area");
+		packageInfo.setExtendsPath("humor");
 		
 		
 		return packageInfo;
@@ -39,26 +39,26 @@ public class BaseTest {
 	private static DatabaseInfo getDbInfo(){
 		DatabaseInfo databaseInfo = new DatabaseInfo();
 		databaseInfo.setDbType("MySQL");
-		databaseInfo.setDbIP("");
-		databaseInfo.setDbName("");
-		databaseInfo.setDbPassword("");
-		databaseInfo.setDbPort();
-		databaseInfo.setDbUsername("");
+		databaseInfo.setDbIP("127.0.0.1");
+		databaseInfo.setDbName("iceyax");
+		databaseInfo.setDbPassword("root");
+		databaseInfo.setDbPort(3306);
+		databaseInfo.setDbUsername("root");
 		return databaseInfo;
 	}
 	
 	private static List<TableInfo> getTables(){
 		List<TableInfo> tables = new ArrayList<>();
 		TableInfo t = new TableInfo();
-		t.setName("t_area");
-		//t.setKey("area_code");//主键字段名称
+		t.setName("t_ice_humor");
+		t.setKey("id");//主键字段名称
 		//t.setKeyType("Single");
 		List<String> query = new ArrayList<>();
 		/*query.add("area_code");
 		query.add("tree_name");
 		query.add("area_name");*/
 		t.setQuery(query);
-		t.setRemark("地区信息表");
+		t.setRemark("笑话幽默表");
 		tables.add(t);
 		return tables;
 	}
